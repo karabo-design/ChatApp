8b8215769a0f5461c9a65352364f56d00563b613
Karab <karab@example.com>
2026-09-13 11:25:42 +0200

    Add registration switch

--- src/Main.java
+++ src/Main.java
@@ -18,3 +18,36 @@ public class Main {
             System.out.print("Please select an option (1-3): ");
 
             String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    // Register Option
+                    System.out.println("\n--- REGISTRATION ---");
+
+                    System.out.print("Enter First Name: ");
+                    String fName = scanner.nextLine();
+
+                    System.out.print("Enter Last Name: ");
+                    String lName = scanner.nextLine();
+
+                    System.out.print("Enter Username (must contain '_' and <= 5 chars): ");
+                    String username = scanner.nextLine();
+
+                    System.out.print("Enter Password (>= 8 chars, 1 capital, 1 number, 1 special): ");
+                    String password = scanner.nextLine();
+
+                    System.out.print("Enter Cell Phone Number (with international code, e.g.,+27838968976):");
+                    String cellNumber = scanner.nextLine();
+
+                    // Call the registration method and print the result
+                    String registrationMessage = loginSystem.registerUser(username, password, cellNumber, fName, lName);
+                    System.out.println("\n" + registrationMessage);
+
+                    if (registrationMessage.contains("successfully")) {
+                        hasRegistered = true;
+                    }
+                    break;
+            }
+        }
+    }
+}
