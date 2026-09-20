da3f32f70c32f9234e11b5bb221e878f8bc72c9a
Karab <karab@example.com>
2026-09-20 15:47:09 +0200

    updated Main, added Login class

---
+++ src/Login.java
@@ -0,0 +1,7 @@
+public class Login {
+    // Variables to store registered user details
+    private String storedUsername;
+    private String storedPassword;
+    private String storedCellNumber;
+    private String firstName;
+    private String lastName;
